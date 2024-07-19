bca1a68c27e7b49419ed2570241e9120239b7bf2
myoungshin <167948199+myoungshin@users.noreply.github.com>
2024-07-19 09:29:22 +0900

    Add files via upload

---
+++ m4_웹 애플리케이션 구축/웹 프로젝트/form.py
@@ -0,0 +1,11 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, TextAreaField, DateField
+# from wtforms.validators import DataRequired
+
+
+# class TaskForm(FlaskForm):
+#     title = StringField("Title", validators=[DataRequired()])
+#     contents = TextAreaField("Contents", validators=[DataRequired()])
+#     due_date = DateField("Due Date", validators=[DataRequired()])
+
+
